9ad697020381aef381ea7476b114129067d5e0e5
Eric Miranda <ericitaquera@gmail.com>
2021-10-21 10:58:07 +0000

    exercise-loops-and-functions.go - Adding precision to the logic.

--- exercise-loops-and-functions.go
+++ exercise-loops-and-functions.go
@@ -1,24 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func Sqrt(x float64) float64 {
-	i := 1
-	root := 0.0
-	for z := 1.0; i < 10; i++ {
-		z -= (z*z - x) / (2 * z)
-		//fmt.Println(z)
-		if z == root {
-			return root
+	z := 1.0
+
+	margin := 0.000000000001
+
+	for {
+		diff := math.Abs(math.Pow(z, 2) - x)
+		if diff < margin {
+			return z
 		}
-		root = z
+		z -= (z*z - x) / (2 * z)
 	}
-	return root
+
+	return z
+
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
-	fmt.Println(Sqrt(1))
-	fmt.Println(Sqrt(3))
-	fmt.Println(Sqrt(4))
+	fmt.Println("minha:", Sqrt(3))
+	fmt.Println("math", math.Sqrt(3))
+	//fmt.Println(Sqrt(1))
+	//fmt.Println(Sqrt(3))
+	//fmt.Println(Sqrt(4))
 }
